d342dc78fbf161329c2e219aad1372e50f8c7dfd
romerojhh <61641738+MrSaladdd@users.noreply.github.com>
2024-11-03 12:44:36 -0800

    Fix camera problem

--- Assets/Scripts/HeightManager.cs
+++ Assets/Scripts/HeightManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class HeightManager : MonoBehaviour
@@ -8,6 +9,7 @@ public class HeightManager : MonoBehaviour
     
     private CharacterController _characterController;
     private SphereCollider _sphereCollider;
+    private XROrigin _xrOrigin;
     [SerializeField] private float desiredHeight;
     [SerializeField] private Transform cameraTransform;
 
@@ -16,6 +18,7 @@ public class HeightManager : MonoBehaviour
     {
         _characterController = GetComponent<CharacterController>();
         _sphereCollider = GetComponent<SphereCollider>();
+        _xrOrigin = GetComponent<XROrigin>();
     }
     
     // Start is called before the first frame update
@@ -25,5 +28,6 @@ public class HeightManager : MonoBehaviour
         _characterController.center.Set(0, desiredHeight / 2f, 0);
         cameraTransform.transform.localPosition = new Vector3(0, desiredHeight, 0);
         _sphereCollider.center.Set(0, desiredHeight, 0);
+        _xrOrigin.CameraYOffset = desiredHeight;
     }
 }
